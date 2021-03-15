c41966a49a5d529d0a7f3cc025ad24b66b1406ef
ryuseitaked <ryutake0306@gmail.com>
2021-03-15 09:46:08 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5e3cfb0ea7fc29b241f81c1d21b03733910cf575336ad818042dd448413a2740a556d450adf0e465bc0ea35db29e6cbf9b9e3b9b7e7869f6c83b5919cf8b94c3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-    root "homes#top"
+  
+   root 'post_images#index'
+   devise_for :users 
+   resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
